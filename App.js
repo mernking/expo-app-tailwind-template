fe5bf81b944b99f7d83214d72f85fbd979b89584
mernking <davtekdon@gmail.com>
2024-08-14 04:45:42 +0100

    ok update

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from "react-native";
+import "./index.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="w-full h-full flex flex-col gap-2 items-center justify-center">
+      <Text className="text-3xl text-blue-500 font-bold">Open up App.js</Text>
+      <Text className="">to start editing</Text>
+      <View className="">
+        <TouchableOpacity
+          className="bg-black text-white font-bold rounded-md px-3 py-2"
+          onPress={() => alert("Button pressed")}
+        >
+          <Text className="text-white">Press me</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./App.js", "./components/**/*.{js,jsx,ts,tsx}"], // Adjust path if necessary
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
